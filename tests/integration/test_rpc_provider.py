a68be0db44478d5fa8d426d99be0c3b24e409f9a
berrytern <berrytern@gmail.com>
2026-02-27 03:46:45 -0300

    feat: update amqp-client-rust to version 0.0.3-alpha.18 and add error handling test for AsyncEventbus

--- tests/integration/test_rpc_provider.py
+++ tests/integration/test_rpc_provider.py
@@ -22,3 +22,25 @@ async def test_provider():
     assert result == bytes(body, "utf-8")
     await eventbus.dispose()
     
+
+
+
+@pytest.mark.asyncio
+async def test_provider_error():
+    options = ConfigOptions(queue_name='test_queue', rpc_exchange_name='test_exchange', rpc_queue_name='test_rpc_queue')
+    eventbus = AsyncEventbus(Config(host='localhost', port=5672, username='guest', password='guest', virtual_host='/', options=options, tls_adaptor=None), QoSConfig(pub_confirm=True, rpc_client_confirm=True, rpc_server_confirm=True, sub_auto_ack=True, rpc_server_auto_ack=True, rpc_client_auto_ack=True, sub_prefetch=None, rpc_server_prefetch=None, rpc_client_prefetch=None))
+    expected_result = "received message"
+    future = Future(loop = get_running_loop())
+    async def handle(body):
+        future.set_result(expected_result)
+        raise Exception("errorad", "adasd")
+    routing_key = "abc.example"
+    body = dumps(["hi"])
+    await eventbus.provide_resource(routing_key, handle)
+    result = await eventbus.rpc_client(options.rpc_exchange_name, routing_key, body, "application/json", ContentEncoding.Null)
+    assert future.done()
+    assert future.result() == expected_result
+    print(result)
+    assert result == bytes("Exception: ('errorad', 'adasd')", "utf-8")
+    await eventbus.dispose()
+    
